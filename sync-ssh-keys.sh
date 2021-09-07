e387f0a25a17ac9479fa984f1c7cdafd24524d4a
Oleksandr Shmyrko <oleksandr.shmyrko@perfectial.com>
2021-09-07 16:49:22 +0300

    Refactoring

--- sync-ssh-keys.sh
+++ sync-ssh-keys.sh
@@ -16,12 +16,13 @@ S3 path should be specified as 'bucket/dir'.
 Options:
     -h, --help      print usage.
     -c, --cronjob   create cronjob to sync keys and script.
-    -d, --debug     enable bash debugging.
-    -s, --sudoers   add sudoers to allow people in group wheel to run all
+    -d, --debug     enable bash debugging (verbose).
+    -s, --sudoers   add sudoers config to allow people in group wheel to run all
                     commands.
 
-To create a cronjob and sync the keys, use the following command:
-    $0 -c bucket/dir1/dir2
+To create a cronjob, add sudoers config and sync the keys, use the following
+command:
+    $0 -cs bucket/dir1/dir2
 
 EOF
 }
@@ -37,17 +38,18 @@ TEMP_DIR=/tmp/ssh-keys
 # ------------------------------------------------------------------------------
 # Log message with prepended date
 log_info() {
-    echo -e "[$(date '+%Y-%m-%d %H:%M:%S')]: $1"
+    echo -e "[$(date '+%Y-%m-%d %H:%M:%S')]: $@"
 }
 
 # Sync public keys from S3 bucket to local directory
 sync_keys() {
+    # --exact-timestamps: https://github.com/aws/aws-cli/issues/3273
     aws s3 sync s3://${S3_PATH} ${TEMP_DIR} \
-        --exclude "*"     \
-        --include "*.pub" \
-        --size-only       \
-        --delete          \
-        --no-progress
+        --exclude "*"      \
+        --include "*.pub"  \
+        --exact-timestamps \
+        --no-progress      \
+        --delete
 }
 
 create_users_with_keys() {
@@ -64,27 +66,27 @@ create_users_with_keys() {
             continue
         fi
 
-        # Create user if not exists, but its public key is present
+        # Create user if it does not exist
         if ! (id -u ${username} &> /dev/null); then
             useradd -m -s /bin/bash -G wheel ${username}
             mkdir -m 700 /home/${username}/.ssh
             log_info "${username} account was created."
-    
-            cp -u ${TEMP_DIR}/${key} /home/${username}/.ssh/authorized_keys
-            chown -R ${username}:${username} /home/${username}/.ssh
-            log_info "${username} public key was added."
-        # Update user's public key in case it was changed (comparison shows changes)
-        elif ! (cmp -s ${TEMP_DIR}/${key} /home/${username}/.ssh/authorized_keys); then
-            cp -u ${TEMP_DIR}/${key} /home/${username}/.ssh/authorized_keys
-            chown -R ${username}:${username} /home/${username}/.ssh
-            log_info "${username} public key was updated."
         fi
+
+        # Copy user's public key in case it is absent or changed
+        case "$(cmp -s ${TEMP_DIR}/${key} /home/${username}/.ssh/authorized_keys; echo $?)" in
+            1|2) install -o ${username} -g wheel -m 600 \
+                    ${TEMP_DIR}/${key} /home/${username}/.ssh/authorized_keys ;;&
+            2)   log_info "${username} public key was added."                 ;;
+            1)   log_info "${username} public key was updated."               ;;
+           #*)   log_info "${username} public key left untouched."            ;;
+        esac
     done
 }
 
 # Delete user in case its public key was deleted
 delete_users_without_keys() {
-    # List of local users (excluding ec2-user, centos and ubuntu)
+    # List of local users (excluding default user)
     # TODO: Consider deleting users whose id > 1000
     local local_users=$(ls /home/ | grep -Evw "$(id -nu 1000)|ec2-user|centos|ubuntu")
 
@@ -99,16 +101,17 @@ delete_users_without_keys() {
 cronjob() {
     local script_name=$(basename $0)
     local script_dir=$(dirname $(realpath $0))
+    local log_path=/var/log/${script_name%.*}.log
 
-    cat <<-EOF > /etc/cron.d/ssh-keys
+    cat <<-EOF > /etc/cron.d/${script_name%.*}
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
 SHELL=/bin/bash
 
 # Sync SSH keys every hour
-0 * * * * root ${script_name} ${S3_PATH} &>> /var/log/ssh-keys.log
+0 * * * * root ${script_name} ${S3_PATH} &>> ${log_path}
 
 # Sync script daily after midnight
-5 0 * * * root aws s3 cp s3://${S3_PATH}/${script_name} ${script_dir} --no-progress &>> /var/log/ssh-keys.log
+5 0 * * * root aws s3 cp s3://${S3_PATH}/${script_name} ${script_dir} --no-progress &>> ${log_path}
 6 0 * * * root chmod 700 $(realpath $0)
 EOF
 
@@ -124,7 +127,7 @@ sudoers() {
 EOF
     chmod 440 /etc/sudoers.d/10-wheel-users
 
-    log_info "Sudoers added."
+    log_info "Sudoers config added."
 }
 
 # ------------------------------------------------------------------------------
@@ -136,7 +139,7 @@ if [ $# -eq 0 ]; then
     exit 2
 fi
 
-# Parse arguments to get options and separate them from script parameters "$@"
+# Parse arguments to get options and separate them from script parameters ($@)
 PARSED_ARGS=$(getopt --name "$(basename $0)" \
                      --options cdhs \
                      --longoptions help,cronjob,debug,sudoers -- "$@")
@@ -159,7 +162,7 @@ while [ $# -ne 0 ]; do
 done
 
 # Handle non-option arguments
-if [[ $# -ne 1 ]]; then
+if [ $# -ne 1 ]; then
     echo -e "$(basename $0): a single S3 path is required (e.g. bucket/dir)"
     exit 1
 else
@@ -169,17 +172,10 @@ fi
 # ------------------------------------------------------------------------------
 # Main
 # ------------------------------------------------------------------------------
-if [ "${DEBUG}" = true ]; then
-    set -o xtrace
-fi
-
-if [ "${CRONJOB}" = true ]; then
-    cronjob
-fi
-
-if [ "${SUDOERS}" = true ]; then
-    sudoers
-fi
+if [ "${DEBUG}"   = true ]; then set -o xtrace; fi
+if [ "${CRONJOB}" = true ]; then cronjob      ; fi
+if [ "${SUDOERS}" = true ]; then sudoers      ; fi
+# TODO: Force recreate users if they were deleted manually
 
 log_info "Starting..."
 sync_keys_output=$(sync_keys)
